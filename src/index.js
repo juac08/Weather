72c5c683dccd7360adef1a5f786fd9034bf8e4b1
juac08 <juni430@gmail.com>
2021-03-28 22:09:50 +0200

    pwa update

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import AppProvider from './context';
-import registerServiceWorker from  './registerServiceWorker';
+import * as registerServiceWorker from  './registerServiceWorker';
 ReactDOM.render(
 
   <AppProvider>
